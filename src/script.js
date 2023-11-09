997a143e2433fc0cad1c1fccc02d6aca1b558efe
Isabelle <loariyafiaba@gmail.com>
2023-11-09 01:29:58 +0000

    Added a responsive navigation bar!

--- src/script.js
+++ src/script.js
@@ -9,3 +9,11 @@ readMoreBtn.addEventListener("click", function(){
     aboutMain.style.height = "100%"; 
     aboutMain.style.overflow = "auto";
 })
+
+// Variables
+const menuIcon = document.querySelector(".main-nav .fa-bars");
+const hiddenNav = document.querySelector(".hidden-nav");
+
+menuIcon.addEventListener("click", () => {
+    hiddenNav.classList.toggle("hide");
+});
